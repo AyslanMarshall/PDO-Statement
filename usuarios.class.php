0aa3de642cc71f256911ff8762d6bd3bebc34b4e
Ayslan Marshall <AyslanMarshall@protonmail.com>
2023-09-19 09:32:12 -0300

    Framework do PDO Statement

---
+++ usuarios.class.php
@@ -0,0 +1,53 @@
+<?php
+
+class Usuarios {
+
+    private $pdo;
+
+    // Conexão com o Banco de Dados
+    public function __construct() {
+        try {
+            $this->pdo = new PDO("mysql:dbname=blog;host=127.0.0.1", "root", "");
+
+        } catch(PDOException $e) {
+            echo "FALHA: ".$e->getMessage();
+        } 
+    }
+    
+    // Selecionandp User
+    public function selecionar($id) {
+        $sql = $this->pdo->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $sql->bindValue(":id", $id);
+        $sql->execute();
+
+        $array = array();
+
+        if($sql->rowCount() > 0) {
+            $array = $sql->fetch(); // retorna so um dado
+        }
+
+        return $array;
+    }
+
+    public function inserir($nome, $email, $senha) {
+        $sql = $this->pdo->prepare("INSERT INTO usuarios SET nome = :nome, email = :email, senha = :senha");
+        $sql->bindParam(":nome", $nome);
+        $sql->bindParam(":email", $email);
+        $sql->bindValue(":senha", md5($senha));
+        $sql->execute();
+    }
+
+    public function atualizar($nome, $email, $senha, $id) {
+        $sql = $this->pdo->prepare("UPDATE usuarios SET nome = ?, email = ?, senha = ? WHERE id = ?");
+        $sql->execute(array($nome, $email, md5($senha), $id));
+    }
+
+    public function excluir($id) {
+        $sql = $this->pdo->prepare("DELETE FROM usuarios WHERE id = ?");
+        $sql->bindValue(1, $id); //pega a primeira ?
+        $sql->execute();
+    }
+}
+
+
+
